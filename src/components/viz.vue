78e8d5521969d3be2f8716dcc58fc58eec86f598
Daniel Corner <dcorn068@gmail.com>
2018-07-01 18:34:58 -0400

    trying to access the simulation for custom forces

--- src/components/viz.vue
+++ src/components/viz.vue
@@ -1,13 +1,13 @@
-<template> 
-   <div> 
-     
-    <d3-network :net-nodes="nodes" :options="options"></d3-network>
+<template>
+   <div>
+
+    <d3-network :net-nodes="nodes" :net-links="[]" :options="options"></d3-network>
 		<!-- <svg id="svgCanvas"> -->
       <!-- <circle v-for="circle in filteredCircles" v-bind:style="{r: circleRadius+'px'}" v-bind:key="circle.id"/> -->
     <!-- </svg> -->
-     
+
    </div>
-</template> 
+</template>
 
 <script>
 //imports
@@ -16,7 +16,7 @@ import dataMixin from '../mixins/dataMixin'
 import * as d3 from 'd3'
 
 export default {
-  components: { 
+  components: {
     D3Network
   },
   // props: {
@@ -28,54 +28,14 @@ export default {
   mixins: [dataMixin],
   data() {
 
-    console.log(dataMixin.data().items);
-    console.log('%c'+{dataMixin},'font-size: 18px; font-weight: bold;');
-    
     return {
-      nodes: [],
-      // links: [],
-      options:
-      {
-        nodeSize: 15,
-        nodeLabels: false,
-        // linkWidth:2,
-        force: 45.01,
-        forces: {
-          Center: false,
-          X: 0.29,
-          Y: 0.29,
-          ManyBody: true,
-          Link: false,
-        },
-        customForces: {
-          cluster: null
-        },
-    // setCustomForces (sim) {
-    //   let forces = this.customForces
-    //   if (forces) {
-    //     for (let f in forces) {
-
-    //       let d3Func = this.getD3Func('force' + f)
-
-    //       if (d3Func) {
-    //         let args = forces[f]
-    //         sim.force('custom' + f, d3Func(...args))
-    //       }
-    //     }
-    //   }
-    //   return sim
-    // },
-    // getD3Func (name) {
-    //   let func = d3[name]
-    //   if (func && typeof (func) === 'function') return func
-    //   return null
-    // },
-      },
-    };
-  },
-  methods: {
-     color: function(cluster){
-       let colors = [
+      nodes: dataMixin.data().items.map(function(d){
+               return {
+                 index: d, x: null, y: null,
+                 cluster: d.cluster,
+                 workers: d.workers,
+               }}),
+      colors: [
           "#4B40DD",
           "#D42A2F",
           "#329E33",
@@ -86,40 +46,78 @@ export default {
           "#7F7F7F",
           "#8B564C",
           "#29BECE",
-       ]
-       return colors[cluster]
+       ],
+
+      // links: [],
+      simulation: null,
+      customForces: {
+        X: 1000,
+      //   Y: 10.29,
+      //   Center: 0,
+      //   // ManyBody: d3.forceManyBody().strength(300),
+      //   // ManyBody: strength(-30),
+      },
+      options: {
+        nodeSize: 15,
+        nodeLabels: false,
+        // linkWidth:2,
+        force: 45.00,
+        forces: {
+          Center: false,
+          X: 0.29,
+          Y: 0.29,
+          ManyBody: true,
+          Link: false,
+        },
+      },
+    };
+  },
+  methods: {
+     color: function(cluster){
+       return this.colors[cluster]
      },
      resizeNodes: function(sizeBy){
       let sizeFunction = null
       switch(sizeBy){
-        case "workers": sizeFunction = (d,i) => this.nodes[i].workers/1800  
+        case "workers": sizeFunction = (d,i) => this.nodes[i].workers/1800
       }
-  
+
       d3.selectAll(".node").transition()
         .delay((d,i)=>i*0.6).duration(250)
         .attr("r", sizeFunction)
      }
   },
-
-  mounted() {
-    // grab the data from the dataMixin
-    this.nodes = dataMixin.data().items
-    console.log(this.nodes)
-    
-    // set the colour
+  created() {
+    const clusters = new Array(this.colors.length)
+   // set the colour
     for(let node of this.nodes){
       node._color = this.color(node.cluster)
     }
+    this.simulation = d3.forceSimulation(this.nodes)
+        .force('cluster', cluster().strength(0.2))
+    //     // .force('charge', d3.forceManyBody().strength(d => -100))
+    //     // .force('link', d3.forceLink(this.graph.links))
+    //     .force('x', d3.forceX(1))
+    //     .force('y', d3.forceY(1))
+  },
+  mounted() {
 
-    console.log('hello');
+    console.log('mounted');
     // grab the simulation
     console.log(D3Network);
+    console.log("%cD3Network.methods.simulate",'font-size: 18px; font-weight: bold;');
+    console.log(D3Network.methods.simulate);
+    console.log("%cD3Network.methods.setCustomForces",'font-size: 18px; font-weight: bold;');
+    console.log(D3Network.methods.setCustomForces);
+    console.log("%cD3Network.methods.getD3Func",'font-size: 18px; font-weight: bold;');
+    console.log(D3Network.methods.getD3Func);
+    console.log(D3Network.methods.getD3Func("forceManyBody"));
 
-    
+
      //--------------- set up the force simulation ---------------\
     // const clusterForce = 3.5;
 
-    // function forceCluster(alpha) { 
+    // function forceCluster(alpha) {
     //   // alpha = attractor force
     //   for (var i = 0, n = nodes.length, node, cluster, k = alpha * 0.12; i < n; ++i) {
     //     node = nodes[i];
@@ -133,7 +131,7 @@ export default {
 
   },
   computed: {
-    
+
     // filteredCircles: function() {
       //   return this.circles.filter(circle => {
         //     return circle > 3;
